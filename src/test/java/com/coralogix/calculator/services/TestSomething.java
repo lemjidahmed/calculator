5ff5aa249957b30857ac5eeb421e0ea2e6f53ef2
Your Name <ahmed.lemjid@etudiant-enit.utm.tn>
2023-04-10 01:37:34 +0100

    b

---
+++ src/test/java/com/coralogix/calculator/services/TestSomething.java
@@ -0,0 +1,16 @@
+package com.coralogix.calculator.services;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TestSomething {
+
+    @Test
+    public void testSomething() {
+        // Perform some test
+        // ...
+
+        // If the test fails, call fail() with a message
+        fail("This test intentionally fails");
+    }
+
+}
