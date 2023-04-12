8ffd6d8010b434d8758aa862e8a214d7c94ad39f
Your Name <ahmed.lemjid@etudiant-enit.utm.tn>
2023-04-12 01:22:42 +0100

    b

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,34 +3,9 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                sh 'mvn clean package'
+                sh 'mvn package -Dsk ipTests\n'
 
-                junit 'target/surefire-reports/*.xml'
-                script {
-                    def failedTests = []
-                    def results = currentBuild.rawBuild.getAction(hudson.tasks.junit.TestResultAction.class).getResult()
-                    results.getFailedTests().each {
-                        failedTests.add(it.getFullName())
-                    }
-                    echo "Failed tests: ${failedTests}"
-                }
 
-                // create a JSON payload for each failed test
-                def bugPayloads = []
-                for (failedTest in failedTests) {
-                    def bugPayload = [:]
-                    bugPayload['title'] = "Bug in test ${failedTest.name}"
-                    bugPayload['status'] = 'Non-Resolved'
-                    bugPayload['description'] = "Test failed with error message: ${failedTest.error}"
-                    bugPayload['projectname'] = projectName
-
-                    bugPayloads.add(bugPayload)
-                }
-
-                // send the JSON payloads to the bug tracker application
-                for (bugPayload in bugPayloads) {
-                    sh "echo '${bugPayload}' | http POST http://localhost:8081/Bug"
-            }
         }
         stage('Test') {
             steps {
