e09d3ba10f62b3310c04c9f6aaa4a7d5afaefddb
Your Name <ahmed.lemjid@etudiant-enit.utm.tn>
2023-04-14 00:30:28 +0100

    b

--- jenkinsfile2.groovy
+++ jenkinsfile2.groovy
@@ -1,57 +1,57 @@
 pipeline {
     agent any
     stages {
-        stage('Build') {
-
-            steps {
-                catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-                    sh 'mvn package'
-                }
-
-            }
-
-        }
         stage('Test') {
             steps {
-                catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-                    sh 'mvn test'
-                }
-
-            }
-        }
-        stage('Report Bugs') {
-            steps {
                 script {
+                    sh 'mvn test'
                     junit 'target/surefire-reports/*.xml'
+                    script {
                         def failedTests = []
                         def results = currentBuild.rawBuild.getAction(hudson.tasks.junit.TestResultAction.class).getResult()
                         results.getFailedTests().each {
                             failedTests.add(it.getFullName())
                         }
                         echo "Failed tests: ${failedTests}"
-                        echo "eeeee"
+                    }
+
+                    // create a JSON payload for each failed test
                     def bugPayloads = []
                     for (failedTest in failedTests) {
                         def bugPayload = [:]
-                        bugPayload['title'] = "Bug in test ${failedTest}"
-                        echo "name test: ${bugPayload['title']}"
+                        bugPayload['id'] = null
+                        bugPayload['title'] = "Bug in test ${failedTest.name}"
                         bugPayload['status'] = 'Non-Resolved'
-                        bugPayload['description'] = "Test failed with error message: ${failedTest}"
-                        bugPayload['projectname'] = "calculator-project"
+                        bugPayload['description'] = "Test failed with error message: ${failedTest.error}"
+                        bugPayload['projectname'] = projectName
+
                         bugPayloads.add(bugPayload)
                     }
-                    echo "Bug Payloads: ${bugPayloads}"
-
-
 
                     // send the JSON payloads to the bug tracker application
                     for (bugPayload in bugPayloads) {
-                        echo "F:${bugPayload}"
-                        sh "echo '${bugPayload}' | http POST http://localhost:8081/Bug"
+                        def bugPayloadJson = new groovy.json.JsonBuilder(bugPayload).toPrettyString()
+
+                        sh "http.post(url: 'http://localhost:8081/Bug', contentType: 'application/json', body: bugPayloadJson)"
+
                     }
                 }
             }
         }
+//        stage('Report Bugs') {
+//            steps {
+//
+//            }
+//        }
+        stage('Build') {
+
+            steps {
+
+                sh 'mvn package'
+            }
+
+        }
+
         stage('Deploy') {
             steps {
                 sh 'mvn deploy'
